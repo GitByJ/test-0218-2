13766db29e642449cf47fdf3abe3df144b12ee6d
Jaejun Shin <jaejunshit@gmail.com>
2018-02-18 20:24:13 +0900

    Initial Commit

---
+++ test 0218 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test 0218 2
+//
+//  Created by Jae-Jun Shin on 18/02/2018.
+//  Copyright © 2018 Jae-Jun Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
